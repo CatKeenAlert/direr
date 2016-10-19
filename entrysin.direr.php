d2667eb0b92acaf822a7fbd616e05108a745bd01
catkeenalert <catkeenalert@gmail.com>
2016-10-19 21:58:47 +0800

    comfortabling for emacs-w3m, optimizing the structure of a table.

--- entrysin.direr.php
+++ entrysin.direr.php
@@ -31,7 +31,7 @@ function generateNodeDetails($nodePath) {
         $nodeDetails = '';
         //$nodeDetails .= '<td>'
         /*由于尚不明确的原因,$openstr要单独提出来求值,直接写入<td></td>中就会跳出<tr></tr>,可能于多层调用或字符连接有关.*/
-        $openStr = is_dir($nodePath) ? '进入目录' : '打开文件';
+        $openStr = is_dir($nodePath) ? '目录' : '文件';
         $nodeDetails .= '<td>'.$openStr.'</td>'."\n";
         $nodeDetails .= '<td>'.$nodeDetailArray['size'].'<td>'."\n";
         $nodeDetails .= '<td>'.$nodeDetailArray['ctime'].'<td>'."\n";
@@ -44,8 +44,8 @@ function generateTable(){
     $inners = scandir($p);
     echo '<table>'."\n";
     echo "<thead><tr>
-<th>文件(夹)</th>
-<th>浏览</th>
+<th>(尾)文件(夹)名</th>
+<th>类型</th>
 <th>文件大小</th>
 <th>创建时间</th>
 <th>修改时间</th>
@@ -59,14 +59,6 @@ function generateTable(){
             $formatName = $value;
             //$formatName = strlen(call_user_func('subStrbywidth', $value, 10));
             $formatName = strlen($formatName) <= 11 ? $formatName : '···'.call_user_func('subStrbywidth', $value, 12);
-            /*
-            $formatName = (substr_count($formatName, '.') <= 2
-                           && $formatName != substr($formatName, strrpos($formatName, '.'))
-                           && $formatName != substr($formatName, strrpos(strrpos($formatName, '.'), '.')))
-                        ? $formatName
-                        : '···---'.substr($formatName, strrpos(strrpos($formatName, '.'), '.'));
-            */
-            //$tableStr .= "<tr><td><a href= $href >".$value.'</a></td>'."\n";
             $tableStr .= "<tr><td><a href= $href >".$formatName.'</a></td>'."\n";
             $tableStr .= call_user_func('generateNodeDetails', $p.'/'.$value);
             $tableStr .= '</tr>'."\n";
