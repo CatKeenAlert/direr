ce4a08f0f7867b6a8f6456f0aa9f47a221610270
catkeenalert <catkeenalert@gmail.com>
2016-10-20 05:33:33 +0800

    对文件ctime和atime的字符串进行了格式化,如本目录图片,现在该项目可在w3m-emacs中轻松愉快的使用,也还算美观. 解决了本人不熟悉elisp所以暂时不会配置打开正在coding的web文件的问题,在emacs分屏模式下相当好用:) 啥时候谢谢README.MD,本人终日在emacs/vim之间穿梭,主要是写php,现代浏览器用得较少,前端的一些效果,可能不会花多少心思去弄,反正在w3m/emacs-w3m下体现不出来.看啥时候写写README.MD . 因为本项目的开发的最初目的是为了实现在emacs/vim中分屏幕的可定制文件浏览web系统,现在已初步实现,短小且精悍.后期会根据实际需要,增加在w3m/emacs-w3m中支持的功能.pathinfo什么的,好像没啥必要.

--- subStrTailByWidth.php
+++ subStrTailByWidth.php
@@ -4,20 +4,15 @@
 function subStrTailByWidth($str, $numLimitSpecial) {
     $employIndex = mb_strlen($str, 'utf8') - 1;
     while
-    (
-              $width + 2 <= $numLimitSpecial  //判断在限定的宽度内还有字符截取名额
-              &&   count($alphaEmployedArray) + 1 <= mb_strlen($str, 'UTF-8')
-    ){
+        (
+            $width + 2 <= $numLimitSpecial  //判断在限定的宽度内还有字符截取名额
+            &&   count($alphaEmployedArray) + 1 <= mb_strlen($str, 'UTF-8')
+        ){
         $alphaEmployedArray[] = mb_substr($str, $employIndex, 1, 'utf8');
         $width = ord(mb_substr($str, $employIndex, 1, 'utf8')) > 127 ? $width + 2 : $width + 1;
-        //echo 'the $width: '.$width.'<br>';
         $employIndex -= 1;
-        //echo 'the $employindex: '.$employIndex.'<br>';
-        //var_dump($alphaEmployedArray);
-        //echo '<br><br>';
     }
 
-    //var_dump($alphaEmployedArray);
     $theString = '';
     for($i = count($alphaEmployedArray) -i; $i >= 0; $i--){
         $theString .= $alphaEmployedArray[$i];
